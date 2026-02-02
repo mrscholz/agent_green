3a6825518bfcfaaa6f9af62956fb83ec02a35024
Markus Scholz <scholz.mr@gmail.com>
2026-02-02 12:05:26 +0100

    Adds cmd flag for verbosity and chat roles

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -11,14 +12,18 @@ else:
     client = genai.Client(api_key=api_key)
     parser = argparse.ArgumentParser(description="AgentGreen")
     parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
-    prompt = args.user_prompt
-    response = client.models.generate_content(model="gemini-2.5-flash", contents=prompt)
+
+
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+    response = client.models.generate_content(model="gemini-2.5-flash", contents=messages)
     if response.usage_metadata == None:
         raise RuntimeError("no respnse from LLM")
     else:
-        print(f"User prompt: {prompt}")
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        if args.verbose:
+            print(f"User prompt: {messages}")
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
         print(f"Response: {response.text}")
     
