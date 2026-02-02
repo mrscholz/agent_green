daa675a50a5cb875b8bba8d18aef637d6b2d9db1
Markus Scholz <scholz.mr@gmail.com>
2026-02-02 12:06:12 +0100

    Accesses file system info

---
+++ functions/get_files_info.py
@@ -0,0 +1,33 @@
+from ntpath import abspath
+import os
+
+
+def get_files_info(working_directory, directory="."):
+    results = []
+    try:
+        abs_path = os.path.abspath(working_directory)
+        target_path = os.path.normpath(os.path.join(abs_path, directory))
+        is_valid_path = os.path.commonpath([abs_path, target_path]) == abs_path
+        path_exists = os.path.isdir(target_path)
+
+        if not is_valid_path:
+            results.append(
+                f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+            )
+        elif not path_exists:
+            results.append(f'Error: "{directory}" is not a directory')
+        else:
+            dir_content = os.listdir(target_path)
+            for item in dir_content:
+                item_path = os.path.normpath(os.path.join(target_path, item))
+                results.append(
+                    f"- {item}: file_size={os.path.getsize(item_path)} bytes, is_dir={os.path.isdir(item_path)}"
+                )
+        return results
+    except Exception as e:
+        results.append(f"Error: {e}")
+        return results
+
+
+if __name__ == "__main__":
+    print(get_files_info("calculator", "."))
