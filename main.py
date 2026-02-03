ae203f8208a4b1440abc8af70f35dc655e2f0f1b
Markus Scholz <scholz.mr@gmail.com>
2026-02-03 16:09:34 +0100

    Provide code to allow the agent a function call.

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import argparse
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from prompts import system_prompt
+from call_function import available_functions
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -15,9 +17,14 @@ else:
     parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
 
-
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
-    response = client.models.generate_content(model="gemini-2.5-flash", contents=messages)
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=messages,
+        config=types.GenerateContentConfig(
+            tools=[available_functions], system_instruction=system_prompt, temperature=0
+        ),
+    )
     if response.usage_metadata == None:
         raise RuntimeError("no respnse from LLM")
     else:
@@ -25,5 +32,8 @@ else:
             print(f"User prompt: {messages}")
             print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
             print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-        print(f"Response: {response.text}")
-    
+        if response.function_calls != None:
+            for function_call in response.function_calls:
+                print(f"Calling function: {function_call.name}({function_call.args})")
+        else:
+            print(f"Response: {response.text}")
