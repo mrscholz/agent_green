23429baabd65e56dc0ea0141879939cd9fec5924
Markus Scholz <scholz.mr@gmail.com>
2026-02-05 10:44:48 +0100

    Lets the agent call functions.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 from prompts import system_prompt
-from call_function import available_functions
+from call_function import available_functions, call_function
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -33,7 +33,28 @@ else:
             print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
             print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
         if response.function_calls != None:
+            function_call_results = []
             for function_call in response.function_calls:
-                print(f"Calling function: {function_call.name}({function_call.args})")
+                # print(f"Calling function: {function_call.name}({function_call.args})")
+                function_call_result = call_function(function_call, args.verbose)
+                if (
+                    function_call_result.parts == None
+                    or len(function_call_result.parts) == 0
+                ):
+                    raise Exception("Error: The parts list appears empty")
+                elif function_call_result.parts[0] == None:
+                    raise Exception("Error: First element of parts is None")
+                elif function_call_result.parts[0].function_response == None:
+                    raise Exception("Error: The function_response is None")
+                elif function_call_result.parts[0].function_response.response == None:
+                    raise Exception("Error: The response element is None")
+                else:
+                    function_call_results.append(
+                        function_call_result.parts[0].function_response.response
+                    )
+                    if args.verbose:
+                        print(
+                            f"-> {function_call_result.parts[0].function_response.response}"
+                        )
         else:
             print(f"Response: {response.text}")
